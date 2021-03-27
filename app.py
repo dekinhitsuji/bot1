574c44ca5096f08d087c60a4fbb1fa6717086828
dekinhitsuji <yattusyuiti@gmail.com>
2021-03-27 14:49:25 +0900

    no message

--- app.py
+++ app.py
@@ -1,58 +1,99 @@
-import configparser
-import json
-from requests_oauthlib import OAuth1Session
-import pandas as pd
-import random
+import tweepy
 import time
-import datetime
+import random
+import configparser
+import schedule
 
 conf = configparser.ConfigParser()
 conf.read('config.ini')
 
-
-API_KEY = conf['twitter']['API_KEY']
-API_SECRET = conf['twitter']['API_SECRET']
+CONSUMER_KEY = conf['twitter']['API_KEY']
+CONSUMER_SECRET = conf['twitter']['API_SECRET']
 ACCESS_TOKEN = conf['twitter']['ACCESS_TOKEN']
-ACCESS_TOKEN_SECRET = conf['twitter']['ACCESS_TOKEN_SECRET']
+ACCESS_SECERET = conf['twitter']['ACCESS_TOKEN_SECRET']
 
-twitter = OAuth1Session(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-url = 'https://api.twitter.com/1.1/statuses/update.json'
 
-df = pd.read_csv('test.csv')
-text = df['ツイート文']
+# consumer　第一引数に(consumer　key)　第二引数に(consumer　secret) #
+auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+# ACCESS_TOKEN_KEY 第一引数に(Access token)　第二引数に(Access token secret) #
+auth.set_access_token(ACCESS_TOKEN,ACCESS_SECERET)
 
-x = len(df)
-list = []
-for i in range(x):
-    ppp = df.at[i,'ツイート文']
+# wait_on_rate_limit = レート制限が補充されるのを自動的に待つかどうか #
+# wait_on_rate_limit_notify = Tweepyがレート制限の補充を待っているときに通知を出力するかどうか #
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-    list.append(ppp)
 
-reserve = []
-for i in range(x):
-    ddd = df.at[i,'ツイート文']
+# screen name (@name)を取得
+my_name = api.me()
+my_screen_name = my_name.screen_name
 
-    reserve.append(ddd)
+
+# 取得したいキーワード #
+list = ['大学　だるい','大学　めんど','当選　したい','当選　お願いします','稼ぎたい','バイト　行かなきゃ',
+                'バイト　行きたくない','バイト　めんどくさい','仕事　辞めたい','免許証　写真','リモート　今日','暇　バイト']
+
+reserve = ['大学　だるい','大学　めんど','当選　したい','当選　お願いします','稼ぎたい','バイト　行かなきゃ',
+                'バイト　行きたくない','バイト　めんどくさい','仕事　辞めたい','免許証　写真','リモート　今日','暇　バイト']
+# ツイート数10件 #
+tweet_count = 100
+
+#自動フォローと判定されにくいようにフォロー間隔 sleep をダンダム値とする
+random_min_time = 4 #sleepでとり得る最小値
+random_max_time = 15 #sleepでとり得る最大値
+
 
 def main():
-    for k in range(x):
+    # for search in search_list:
+    global list
+    global reserve
+    if list == 0:
+        list = reserve
+    search_list = random.choice(list)
+    search = search_list
+    liked_count = 0 #いいねカウンター
 
-        #ツイートをランダムで選択
-        tweet = random.choice(list)
+    print(my_screen_name,': ','------------------------------')
+    print(my_screen_name,': ','Searching... {}' .format(search))
+    # サーチ結果を格納
+    search_result = api.search(q=search, count=tweet_count)
 
-        #Twitterにツイートを送信
-        params = {'status':tweet}
-        response = twitter.post(url, params=params)
-        
+    for tweet in search_result:
+        tweet_id = tweet.id
+        user_name = tweet.user._json['name']
+        user_screen_name = tweet.user._json['screen_name']
+        tweet_status = api.get_status(tweet_id)
+        tweet_favorite_status = tweet_status.favorited
 
-        #成功したか否か
-        print(response.status_code)
-
-        #使用したものをリストから消去
-        list.remove(tweet)
-
-
-        time.sleep(3600)
+        check_status = 'False'
+        tweet_favorite_status_01 = str(tweet_favorite_status)
 
 
-main()
+        if tweet_favorite_status_01 == check_status:
+            if liked_count == 12:
+                continue
+            try:
+                api.create_favorite(id=tweet_id)   # いいねの処理
+            except tweepy.TweepError as e:
+                print(e.reason)
+            except StopIteration:
+                continue
+            tweet_status = api.get_status(tweet_id)
+            tweet_favorite_status = tweet_status.favorited
+            print(my_screen_name,': ',user_name,'(',user_screen_name,")","いいねしました(STATUS =",tweet_favorite_status,")")
+            liked_count += 1
+            sleep_random_time = random.randint(random_min_time,random_max_time)
+            print(my_screen_name,': ',' 次のいいねまで' ,sleep_random_time,'秒待ちます')
+            time.sleep(sleep_random_time)
+        else:
+            print(my_screen_name,': ',user_name,'(',user_screen_name,')','はいいね済み(',tweet_favorite_status,')なのでスキップしました')
+
+    print(my_screen_name,": ","search =",search,"で",liked_count,"人いいねしました。終了します。")
+    list.remove(search)
+
+
+schedule.every(2).hours.do(main)
+
+
+while True:
+  schedule.run_pending()
+  time.sleep(60)
